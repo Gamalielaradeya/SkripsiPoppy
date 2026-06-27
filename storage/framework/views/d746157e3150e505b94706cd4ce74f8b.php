52b804ab3754333c5f95f306ce3aab358f9f03fd
Gamaliel <gamaliel@localhost>
2026-06-27 06:06:19 +0000

    chore: add .env.snapshot backup

--- storage/framework/views/d746157e3150e505b94706cd4ce74f8b.php
+++
@@ -1,9 +0,0 @@
-<section <?php echo e($attributes->merge(['class' => 'overflow-hidden rounded-lg border border-slate-200 bg-white shadow-sm'])); ?>>
-    <div class="overflow-x-auto">
-        <table class="min-w-full divide-y divide-slate-200 text-sm">
-            <?php echo e($slot); ?>
-
-        </table>
-    </div>
-</section>
-<?php /**PATH /var/www/skripsi-poppy/resources/views/components/data-table.blade.php ENDPATH**/ ?>
